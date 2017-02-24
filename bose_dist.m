c2ffc4e634bbeabc57c9a3ca1684365737d00cf0
spicydonkey <dk.shin1992@gmail.com>
2017-02-24 23:21:02 +1100

    Fix error in formula

--- bose_dist.m
+++ bose_dist.m
@@ -5,5 +5,5 @@ function n_bose = bose_dist(params, k)
     Nth=params(1);
     Ta=params(2);
     
-    n_bose=Nth*g_bose(exp(-6.0597e-20*(k.^2)/Ta))/(4.4870e-29*(Ta^(3/2)));
+    n_bose=Nth*g_bose(exp(-6.0597e-20*(k.^2)/Ta))/(4.4870e29*(Ta^(3/2)));
 end
