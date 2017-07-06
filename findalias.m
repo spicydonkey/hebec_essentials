972ef1debd094d7294eef912575391fc593ec13c
David <dk.shin1992@gmail.com>
2017-07-07 09:50:24 +1000

    debug alias code for 1-hit halo + xy diffs

--- findalias.m
+++ findalias.m
@@ -2,29 +2,48 @@
 %
 % txy : Nx3 array of [T,X,Y] vectors
 % dt_alias: aliasing dead-time in seconds (default 100 ns)
+% dxy_alias: diff in X,Y to distinguish separate counts (default Inf)
 %
 % bool_alias : Nx1 boolean array to indicate aliased points
 
-function bool_alias = findalias(txy,dt_alias)
+function bool_alias = findalias(txy,dt_alias,dxy_alias)
 % check inputs
 if ~exist('dt_alias','var')
     dt_alias=100e-9;        % Default: 100 ns dead-time (Sean's thesis)
 end
+if ~exist('dxy_alias','var')
+    dxy_alias=Inf;          % Default: reject all counts if dt in deadtime
+end
 
 % check if T is sorted
+Isort=[];
 if ~issorted(txy(:,1))
     % T isn't sorted! might take long
     warning('Points are not ordered in time! this may take a while...');
     
     % TODO - sort txy according to T
+    [txy,Isort]=sortrows(txy,1);    % txy is sorted from here!
+    [~,Irev]=sort(Isort);           % reverse sort back to original
 end
+% T is sorted!
 
-% diffs in time of arrival
-dt=diff(txy(:,1));
+% diffs in time of arrival & spatial
+dt=diff(txy(:,1),1,1);          
+dxy=diff(txy(:,2:3),1,1);       % diff in xy for "T-neighbouring counts"
 
 % check alias!
-% TODO - check dX and dY!
-bool_alias=(dt<dt_alias);
+bool_alias=(dt<dt_alias);                   % proximity in time
+bool_nearxy=(max(dxy,[],2)<dxy_alias);      % proximity in XY
+
 bool_alias=[false;bool_alias];      % define "FIRST HIT" as genuine
+bool_nearxy=[false;bool_nearxy];
+
+% discounts only when two hits are BOTH near in TIME and SPACE
+bool_alias=bool_alias&bool_nearxy;
+
+% handle index scrambled data from sort
+if ~isempty(Isort)
+    bool_alias=bool_alias(Irev);    % reverse back to original data indexing
+end
 
 end
