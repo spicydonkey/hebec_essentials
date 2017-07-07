bca5afff2a11a843ff9a8c6f7f909df4a5108ce5
David <dk.shin1992@gmail.com>
2017-07-07 12:16:47 +1000

    new funciton: zxy --> radial dist with centre

---
+++ zxy2rdist.m
@@ -0,0 +1,16 @@
+% ZXY to radial distance
+
+function r = zxy2rdist(zxy,cent)
+if ~exist('cent','var')
+    cent=zeros(3,1);
+end
+
+ncounts=size(zxy,1);
+
+% offset centre
+zxy0=zxy-repmat(cent,[ncounts,1]);
+
+% evaluate radial distance (from centre)
+r=sqrt(sum((zxy0.^2),2));
+
+end
