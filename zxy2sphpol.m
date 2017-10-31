e0f0d02e210e7a37d100e7e0a7f92e0bb6d5bf76
David <dk.shin1992@gmail.com>
2017-10-31 17:56:28 +1100

    new function: cart 2 sphpol in matlab convention

---
+++ zxy2sphpol.m
@@ -0,0 +1,15 @@
+function sphpol = zxy2sphpol(zxy)
+% SPHPOL = ZXY2SPHPOL(ZXY)
+%
+% Converts counts in Cartesian coordinate to Spherical coord
+%
+% I/O
+% ZXY: should be a Nx3 array
+%
+% SPHPOL: Nx3 array of sph polar coord [AZIM,ELEV,NORM] (Matlab convention)
+
+rad=sqrt(sum(zxy.^2,2));    % radius
+azim=atan2(zxy(:,3),zxy(:,2));  % azimuthal ang=atan2(Y/X) - 4-quadrant inverse tangent
+elev=asin(zxy(:,1)./rad);       % elevation ang=asin(Z/NORM)
+
+sphpol=[azim, elev, rad];   % SPHPOL=(azim,elev,R)
