7cc4a4a7b7ffd9c0c54864324db76aaeb7c1cf75
David <dk.shin1992@gmail.com>
2017-11-12 14:30:42 +1100

    comment code

--- zxy2rdist.m
+++ zxy2rdist.m
@@ -1,6 +1,13 @@
-% ZXY to radial distance
-
 function r = zxy2rdist(zxy,cent)
+% ZXY to radial distance
+%
+% R = ZXY2RDIST(ZXY)
+% R = ZXY2RDIST(ZXY, CENT)
+%
+% ZXY: Nx3 array of vectors
+% CENT: 1x3 array for center
+%
+
 if ~exist('cent','var')
     cent=zeros(1,3);
 end
