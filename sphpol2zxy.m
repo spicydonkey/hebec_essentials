e6f22c13b571e4bd2ef005cca988b8f0c73991c9
David <dk.shin1992@gmail.com>
2017-11-23 13:52:29 +1100

    sph-polar counts to ZXY experiment friendly form

---
+++ sphpol2zxy.m
@@ -0,0 +1,14 @@
+function kk=sphpol2zxy(ss)
+% Converts spherical-polar vectors to Cart coords (ZXY experiment form)
+%
+% KK = SPHPOL2ZXY(SS)
+%
+% SS: Nx3 array of form [az,el,rad]
+% KK: Nx3 array of form [Z,X,Y]
+%
+
+% use matlab's default function to transform spol --> cart
+kk=sph2cart(ss(:,1),ss(:,2),ss(:,3));   % in XYZ form
+kk=circshift(kk,1,2);       % XYZ --> ZXY
+
+end
