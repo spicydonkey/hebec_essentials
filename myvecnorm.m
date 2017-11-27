c27575ef9556b822dc66ed692ca81dfa662b822d
David <dk.shin199@gmail.com>
2017-11-28 10:24:44 +1100

    update myvecnorm: generalise vector dimension

--- myvecnorm.m
+++ myvecnorm.m
@@ -1,9 +1,15 @@
-function norms=myvecnorm(vecs)
-% NORMS = MYVECNORM(VECS)
+function norms=myvecnorm(vecs,dim)
+% NORMS = MYVECNORM(VECS,DIM)
 % Evaluates Cartesian norm of N-dimensional vectors
 %
-% NORMS: vector norms
-% VECS: N-by-M array of M-dimensional vectors
+% NORMS:    vector norms
+% VECS:     N-by-M array of M-dimensional vectors
+% DIM:      (OPT) array dimension in which the N-dim vector is in (default is 2)
 %
 
-norms=sqrt(sum(vecs.^2,2));
+if ~exist('dim','var')
+    dim=2;  % default is N-dim "row (dim=2)" vectors 
+end
+
+
+norms=sqrt(sum(vecs.^2,dim));
