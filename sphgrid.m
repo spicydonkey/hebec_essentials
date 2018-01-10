56c709411601503b76ab7ec9c434cafe7fcc0e8a
David <dk.shin1992@gmail.com>
2018-01-11 10:07:13 +1100

    new function to create lat-lon grids on sphere

---
+++ sphgrid.m
@@ -0,0 +1,29 @@
+function [az,el] = sphgrid(naz,nel,az_lim,el_lim)
+%SPHGRID construct 2D lat-lon grid on sphere.
+%   [AZ,EL] = SPHGRID(NAZ,NEL)
+%   [AZ,EL] = SPHGRID(NAZ,NEL,AZ_LIM,EL_LIM)
+%
+%   Grid are in linearly divisions of lat-lon lines on a sphere in radians.
+%
+%   NB: NAZ should be even for the constructed grid to be invertible.
+%   Parity of NEL has no effect.
+%   
+
+% check args
+if ~exist('az_lim','var')
+    az_lim=[-pi,pi];
+end
+if ~exist('el_lim','var')
+    el_lim=[-pi/2,pi/2];
+end
+
+% 1D linearly spaced lat/lon
+az1=linspace(az_lim(1),az_lim(2),naz+1);
+az1=az1(1:end-1);       % azimuthal angle will wrap in default usage
+
+el1=linspace(el_lim(1),el_lim(2),nel);
+
+% create 2D grid of lat-lon points
+[az,el]=ndgrid(az1,el1);
+
+end
