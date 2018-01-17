f894e8df59abedb339beaba39acc5a3ee8dd30e3
David <dk.shin1992@gmail.com>
2018-01-17 14:09:02 +1100

    more general vector norm function

---
+++ vecnorm.m
@@ -0,0 +1,15 @@
+function r = vecnorm(v,dim)
+%Evaluates norms of vectors
+%
+%   v: vector array
+%   dim: vector dimension (default: 2 - row vecs)
+%
+
+if ~exist('dim','var')
+    % row-vector as default
+    dim=2;
+end
+
+r=sqrt(sum(v.^2,dim));
+
+end
