0a97531d858cc1c748b7e6028a1bf8f8e33f2e27
David <dk.shin1992@gmail.com>
2018-01-17 14:13:02 +1100

    filter for counts in extended cone

---
+++ crop/inCone.m
@@ -0,0 +1,24 @@
+function [v_in,b_in]=inCone(zxy,az,el,theta)
+%Get counts in an extended cone region.
+%
+%   zxy: array of Cart row vectors
+%   az: cone-axis azim angle [rad]
+%   el: cone-axis elev angle [rad]
+%   theta: cone half-angle
+%
+%   v_in: C-vecs in cone
+%   b_in: boolean array for vecs in cone
+%
+
+
+% transform Caresian --> spherical
+vsph=zxy2sphpol(zxy);
+
+% get rel angles of vecs to cone
+dtheta=diffAngleSph(vsph(:,1),vsph(:,2),az,el);
+
+% filter vecs in cone
+b_in=dtheta<theta;
+v_in=zxy(b_in,:);
+
+end
