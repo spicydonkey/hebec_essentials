3ef3e76345ee7d3591c30daada10a0af6caf5081
David <dk.shin1992@gmail.com>
2018-01-17 14:09:55 +1100

    radial filter wrapper for cell-array

---
+++ cfilter_norm.m
@@ -0,0 +1,7 @@
+function xf = cfilter_norm(x,rmin,rmax)
+%xf = cfilter_norm(x,rmin,rmax)
+%
+%   filter_norm wrapper for cell-array dataset
+%
+
+xf=cellfun(@(C) filter_norm(C,rmin,rmax),x,'UniformOutput',false);
