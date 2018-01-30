23e5a3e5bc37e8fcacac5a0ac0bd11534fb71b23
David <dk.shin1992@gmail.com>
2018-01-30 12:21:10 +1100

    tidy codebase

--- boost_zxy.m
+++ boost_zxy.m
@@ -1,17 +1,13 @@
-function zxy_f = boost_zxy(zxy_i,dzxy)
-% Add a constant offset to all particle distribution (re-centering)
+function V = boost_zxy(U,du)
+%Apply uniform offset to all shots.
+%   - Useful for re-centering experimental data.
 %
-% ZXY_F = BOOST_ZXY(ZXY_I,DZXY)
+% V = BOOST_ZXY(U,du)
 %
-%
-% ZXY_I:    cell-array of zxy counts
-% DZXY:     1x3 zxy vector to shift
+%   U: shots of vectors
+%   du: offset vector
 %
 
-zxy_f=cell(size(zxy_i));
-nshot=size(zxy_i,1);
-for ii=1:nshot
-    n=size(zxy_i{ii},1);   % num of counts in this shot
-    zxy_f{ii}=zxy_i{ii}+repmat(dzxy,n,1);   % boost in cartesian
-end
+V=cellfun(@(x) x+du,U,'UniformOutput',false);
+
 end
--- crop/boxcull.m
+++ crop/boxcull.m
@@ -1,44 +1,43 @@
-% Crop N X M-dim array to a box
-% 
-% DKS 31/01/17
+function [v_out,I_out,n_out,cpos,se_cpos,sd_pos] = boxcull(v_in,box_lim)
+%Extract vectors lying inside a box.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % INPUT
-%   * array_in      - N by M array (usage: N x data vectors of dim M)
+%   * v_in      - N by M array (usage: N x data vectors of dim M)
 %   * box_lim       - 1xM cell array of box limits for each dim
 % OUTPUT
-%   * array_out     - array of vectors within box
-%   * ind_out       - indices of vectors (rows) in box
+%   * v_out     - array of vectors within box
+%   * I_out       - indices of vectors (rows) in box
 %   * n_out         - number of points captured
 %   * cpos          - centre position (mean): [xc,yc,zc,...]
 %   * se_cpos       - standard error of mean position
 %   * sd_pos        - rms width of captured counts
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DKS 31/01/17
 
-function [array_out,ind_out,n_out,cpos,se_cpos,sd_pos] = boxcull(array_in,box_lim)
-nvects=size(array_in,1);
-ind_out=1:nvects;
+nvects=size(v_in,1);
+I_out=1:nvects;
 
-array_out=array_in;         % initialise output
+v_out=v_in;         % initialise output
 
-ndim=size(array_in,2);
+ndim=size(v_in,2);
 for i=1:ndim
     if isempty(box_lim{i})  % pass crop if empty
         continue;
     end
-    in_window=((array_out(:,i)>box_lim{i}(1))&(array_out(:,i)<box_lim{i}(2)));
-    array_out=array_out(in_window,:);       % crop all remaining to this component
-    ind_out=ind_out(in_window);             % update remaining data's parent indices
+    in_window=((v_out(:,i)>box_lim{i}(1))&(v_out(:,i)<box_lim{i}(2)));
+    v_out=v_out(in_window,:);       % crop all remaining to this component
+    I_out=I_out(in_window);             % update remaining data's parent indices
 end
 
 % evaluate number of captured counts
-n_out=size(array_out,1);
+n_out=size(v_out,1);
 
 % evaluate mean position of captured counts
-cpos=mean(array_out,1);
+cpos=mean(v_out,1);
 
 % spread/width
-sd_pos=std(array_out,1);        % rms width in each dim
+sd_pos=std(v_out,1);        % rms width in each dim
 se_cpos=sd_pos/sqrt(n_out);     % se
 
 end
