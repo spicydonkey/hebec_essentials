f78f1bf8696e6cd22e7caac17eb0cb62ec2dfda3
David <dk.shin1992@gmail.com>
2018-05-24 18:59:28 +1000

    bug fix and tidy code to read csv file from DMM

--- instrument/read_DMM_log.m
+++ instrument/read_DMM_log.m
@@ -1,6 +1,9 @@
 % Read measurement log from Tektronic DMM
 % DK Shin
-% 16/06/2016
+% 2016-06-16: first implementation
+% 2018-05-24: bug fix: smarter way of looking for processing datetime and cell
+%   formatting
+
 
 function DATAOUT = read_DMM_log(filename)
 
@@ -15,25 +18,32 @@ for iLine = 1:nlines
 end
 fclose(fid);
 
-DATAOUT=zeros(nSamp,2);    % initialise data array
+data_collated=vertcat(DATA_CELL{:});
+b_nonempty_cell=cellfun(@(c) ~isempty(c),data_collated);
+
+idx_col_datetime=find(b_nonempty_cell(1,:),1);      % index which datetime info starts
+idx_col_data=find(b_nonempty_cell(2,:),1);      % index which data is
+
 
 % calculate time-stamp for measurement data
-t_start = DATA_CELL{1}{7};
-t_start = regexp(t_start, ':', 'split');
-t_start = 3600*str2num(t_start{1}) + 60*str2num(t_start{2}) + str2num(t_start{3});
+dd=data_collated{1,idx_col_datetime};
+tt=data_collated{1,idx_col_datetime+1};
+t_start_datetime=datetime([dd,'T',tt],...
+    'InputFormat','dd-MM-yy''T''HH:mm:ss');
 
-t_end = DATA_CELL{nlines}{7};
-t_end = regexp(t_end, ':', 'split');
-t_end = 3600*str2num(t_end{1}) + 60*str2num(t_end{2}) + str2num(t_end{3});
+dd=data_collated{end,idx_col_datetime};
+tt=data_collated{end,idx_col_datetime+1};
+t_end_datetime=datetime([dd,'T',tt],...
+    'InputFormat','dd-MM-yy''T''HH:mm:ss');
 
-% check if on different day (assuming a log must be strictly less than 24 hours)
-if t_end < t_start
-    t_end = t_end + 3600*24;
-end
+t_dur=t_end_datetime-t_start_datetime;      % duration of experiment
+t_exp=linspace(0,seconds(t_dur),nSamp);     % time points for exp data in seconds
 
-DATAOUT(:,1)=linspace(0,t_end-t_start,nSamp);
 
 % get measurement data
-for iSamp=1:nSamp
-    DATAOUT(iSamp,2)=str2double(DATA_CELL{iSamp+1}{2});
+DATAOUT=NaN(nSamp,2);    % initialise data array
+DATAOUT(:,1)=t_exp;
+DATAOUT(:,2)=cellfun(@(c) str2double(c),data_collated(2:end-1,idx_col_data));
+
+
 end
