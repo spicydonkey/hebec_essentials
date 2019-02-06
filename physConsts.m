abb4507bd5743579d45a36f6805f0b4861ff0e01
David <dk.shin1992@gmail.com>
2019-02-06 17:38:59 +1100

    update physConsts

--- physConsts.m
+++ physConsts.m
@@ -3,6 +3,10 @@ function s = physConsts()
 %
 %   Physical constants relevant to metastable helium experiments
 %
+% All values are in SI units
+%
+% DKS
+%
 
 s.Na=6e23;
 % s.m_He=4/s.Na/1000;
@@ -15,6 +19,7 @@ s.mu_He=2.8e6;
 s.hbar=1.05457e-34;
 s.kB=1.38066e-23;
 s.k_He=(2*pi)/s.lambda_He;
-s.g=9.81;
+s.g=9.81;                   
+s.a_11=7.512e-9;            % s-wave scat length spin-polarised He* [Moal, et. al., PRL (96) 2006]
 
 end
