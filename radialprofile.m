c99d0be6fbe7128dd02841820e3641f09f863c06
David <dk.shin1992@gmail.com>
2019-03-09 01:00:51 +1100

    tidy code

--- radialprofile.m
+++ radialprofile.m
@@ -1,35 +1,42 @@
-function [nr,rc]=radialprofile(r,red,verbose)
+function [n_r,r_cent,b]=radialprofile(r_data,r_ed,verbose)
 %Get radial distribution normalised by r-dependent bin volume
 %
-%   r: array of vector norms
+%   r_data: array of vector norms
 %
-%   nr: radial density profile [arb. u.]
-%   rc: bin centers
+%   n_r: radial density profile [arb. u.]
+%   r_cent: bin centers
+%   b: handle to bar/stairs plot
 %
 
 % input check
 if ~exist('verbose','var')
     verbose=1;      % default is verbose
 end
-if ~exist('red','var')
-    red=linspace(min(r),max(r),20);
+if ~exist('r_ed','var')
+    r_ed=linspace(min(r_data),max(r_data),20);
 end
 
 % histogram counts
-nr=histcounts(r,red);
+N_r=histcounts(r_data,r_ed);
 
-% get bin properties
-rc=red(1:end-1)+0.5*diff(red);
+% get bin centers
+r_cent=r_ed(1:end-1)+0.5*diff(r_ed);
 
-% get normalised density
-Vbin=1./(rc.^2);    % radially dependent bin volume (proportionality can be ignored)
-nr=nr./Vbin;
-nr=nr/max(nr);      % normalise max to 1
+% normalise radially dependent bin volume = 4/3*pi*(r2^3 -r1^3)
+vol_sph = @(r) 4/3*pi*r.^3;
+vol_r_bin=diff(vol_sph(r_ed));      % exact bin volume
+n_r=N_r./vol_r_bin;                 % normalise by bin vol
+
+
+n_r=n_r./(sum(n_r)*diff(r_ed));     % norm- bin-size
+n_r=n_r/max(n_r);       % normalies by max
 
 % histogram plot
 if verbose>0
-    figure;
-    b=bar(rc,nr,'histc');
+    b=stairs(r_cent,n_r);
+    
+    box on;
+    xlabel('r');
+    ylabel('P');
 end
-
 end
